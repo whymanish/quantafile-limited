52a4eba32704f5b62b859e64bf2b733515e9d094
Manish <my79220@gmail.com>
2023-12-23 21:21:25 +0530

    added

---
+++ app/components/HeroCards.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const HeroCards = ({ title, category, imageUrl, date }) => (
+    <div className="relative snap-center shrink-0">
+      <div className="w-[400px] lg:w-[420px] transition-all duration-200 transform bg-white border border-gray-200 rounded-2xl hover:shadow-lg hover:-translate-y-1">
+        <div className="px-4 py-5 sm:p-5">
+          <div className="flex items-center">
+            <a href="#" title="" className="shrink-0">
+              <img className="lg:h-24 w-14 h-14 lg:w-24 rounded-xl object-cover" src={imageUrl} alt="" />
+            </a>
+  
+            <div className="flex-1 ml-4 lg:ml-6">
+              <p className="text-xs font-medium text-gray-900 lg:text-sm">
+                <a href="#" title="" className="">
+                  {category}
+                </a>
+              </p>
+              <p className="mt-2 text-sm font-bold text-gray-900 lg:text-lg group-hover:text-gray-600">
+                <a href="#" title="" className="">
+                  {title}
+                </a>
+              </p>
+              <p className="mt-2 text-xs font-medium text-gray-500 lg:text-sm">{date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+export default HeroCards
