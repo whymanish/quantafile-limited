28990a4ae235fd5a19bd507f80ba7918f22a63bc
manish <my79220@gmail.com>
2023-12-24 15:32:59 +0530

    added

--- app/components/Service/Service.jsx
+++ app/components/Service/Service.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import PriceCard from "./PriceCard";
 import Feature from "./Feature";
-
 const Service = () => {
   const commonFeatures = [
     "Premium Component",
@@ -11,22 +10,31 @@ const Service = () => {
     "Customer Support",
   ];
 
+  const backgroundImage = {
+    backgroundImage: "url('https://images.unsplash.com/photo-1637825891028-564f672aa42c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-  <div className="flex flex-col items-center justify-center min-h-screen pb-10 bg-[#0e1218]">       <section>
+    <section style={backgroundImage}>
+  <div className="flex flex-col items-center justify-center min-h-screen pb-10 ">
+    
         <div className="mx-auto max-w-7xl px-5  md:px-10">
           <div className="mx-auto mb-12 mt-24 lg:mt-0  max-w-3xl text-center md:mb-12">
-            <h2 className="text-3xl  font-bold text-white md:text-5xl">WebApps Development</h2>
-            <p className="mt-4 text-sm text-gray-200 sm:text-base">
+            <h2 className="text-3xl  font-bold text-gray-50 md:text-5xl">WebApps Development</h2>
+            <p className="mt-4 text-sm text-gray-100 sm:text-base">
               Simple &amp; fixed pricing. 30 days money-back guarantee
-            </p>s
+            </p>
           </div>
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:gap-4">
             <PriceCard
               title="1"
               price="UI/UX & Frontend Development"
               bgColor="overflow-hidden rounded-3xl bg-gradient-to-r from-gray-100 to-blue-50 border-2 p-10 text-white sm:w-full sm:flex-initial"
-              btnBgColor="bg-black"
-              btnTextColor="text-white"
+              btnBgColor="bg-white border shadow-xl"
+              btnTextColor="text-black"
               features="Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam, purus sit"
             >
               {commonFeatures.map((feature, index) => (
@@ -38,8 +46,8 @@ const Service = () => {
               title="2"
               price="Backend Development"
               bgColor="overflow-hidden rounded-3xl bg-gradient-to-r from-gray-100 to-blue-50 border-2 p-10 border-2 p-10 text-white sm:w-full sm:flex-initial"
-              btnBgColor="bg-black"
-              btnTextColor="text-white"
+              btnBgColor="bg-white border shadow-xl"
+              btnTextColor="text-black"
               features="Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam, purus sit"
             >
               {commonFeatures.map((feature, index) => (
@@ -51,8 +59,8 @@ const Service = () => {
               title="3"
               price="Fullstack Development"
               bgColor="overflow-hidden rounded-3xl bg-gradient-to-r from-gray-100 to-blue-50 border-2 p-10 border-2 p-10 text-white sm:w-full sm:flex-initial"
-              btnBgColor="bg-black"
-              btnTextColor="text-white"
+              btnBgColor="bg-white border shadow-xl"
+              btnTextColor="text-black"
               features="Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam, purus sit"
             >
               {commonFeatures.map((feature, index) => (
@@ -61,8 +69,9 @@ const Service = () => {
             </PriceCard>
           </div>
         </div>
-      </section>
+     
     </div>
+    </section>
   );
 };
 
